016fd04896fe8818e33511df1135bfe5f02947b1
pixelpartner <thomas@pixelpartner>
2011-10-04 10:55:50 +0200

    Revert d09e99b232590e3431efa323ecdaadfe5d542054^..HEAD

--- samples/RL-ou_KMQ1280.sh
+++ samples/RL-ou_KMQ1280.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
-ffmpeg -y -i "/Users/thomas/Documents/trueColor3D/samples/RL-ou.mp4" -aspect 1280:1040 -vf "[in]select=1,crop='iw:ih/2:0:ih/2',scale=1040:588[A];movie=/Users/thomas/Documents/trueColor3D/samples/RL-ou.mp4,select=1,crop='iw:ih/2:0:0',scale=1040:588,pad=1040:1280:0:0:black[B];[B][A]overlay=0:692,transpose=1" -f mp4 "/Users/thomas/Documents/trueColor3D/samples/RL-ou_KMQ1280.tmp"
+ffmpeg -y -i "/Users/thomas/Documents/trueColor3D/samples/RL-ou.mp4" -b 8500K -aspect 1280:1040 -vf "[in]select=1,crop='iw:ih/2:0:ih/2',scale=1040:588[A];movie=/Users/thomas/Documents/trueColor3D/samples/RL-ou.mp4,select=1,crop='iw:ih/2:0:0',scale=1040:588,pad=1040:1280:0:0:black[B];[B][A]overlay=0:692,transpose=1" -f mp4 "/Users/thomas/Documents/trueColor3D/samples/RL-ou_KMQ1280.tmp"
 qt-faststart "/Users/thomas/Documents/trueColor3D/samples/RL-ou_KMQ1280.tmp" "/Users/thomas/Documents/trueColor3D/samples/RL-ou_KMQ1280.mp4"
 rm "/Users/thomas/Documents/trueColor3D/samples/RL-ou_KMQ1280.tmp"
--- samples/RL-ou_my3D720p.sh
+++ samples/RL-ou_my3D720p.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
-ffmpeg -y -i "/Users/thomas/Documents/trueColor3D/samples/RL-ou.mp4" -b 8500K -aspect 2.66667 -vf "[in]select=1,crop='iw:ih/2:0:ih/2',scale=874:491,pad=1920:720:10:114:black[A];movie=/Users/thomas/Documents/trueColor3D/samples/RL-ou.mp4,select=1,crop='iw:ih/2:0:0',scale=874:491[B];[A][B]overlay=936:114" -f mp4 "/Users/thomas/Documents/trueColor3D/samples/RL-ou_my3D720p.tmp"
+ffmpeg -y -i "/Users/thomas/Documents/trueColor3D/samples/RL-ou.mp4" -b 8500K -aspect 2.66667 -vf "[in]select=1,crop='iw/2:ih:iw/2:0',scale=874:491,pad=1920:720:10:114:black[A];movie=/Users/thomas/Documents/trueColor3D/samples/RL-ou.mp4,select=1,crop='iw/2:ih:0:0',scale=874:491[B];[A][B]overlay=936:114" -f mp4 "/Users/thomas/Documents/trueColor3D/samples/RL-ou_my3D720p.tmp"
 qt-faststart "/Users/thomas/Documents/trueColor3D/samples/RL-ou_my3D720p.tmp" "/Users/thomas/Documents/trueColor3D/samples/RL-ou_my3D720p.mp4"
 rm "/Users/thomas/Documents/trueColor3D/samples/RL-ou_my3D720p.tmp"
